9fc1d10793d90b6f95dc00eb9b73fbc9173ea40c
Luis Arrufat <l.arrufat@defacto.aero>
2021-04-08 18:12:23 +0200

    Agregar archivos de proyecto.

---
+++ CloudApps/ConfigurationValues.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ServerApps {
+    public class ConfigurationValues {
+        public DateTime date { get; set; }
+
+        public int ID { get; set; }
+
+        public string description { get; set; }
+    }
+}
---
+++ CloudApps/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerApps.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase {
+
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger) {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<ConfigurationValues> Get() {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new ConfigurationValues {
+                date = DateTime.Now.AddDays(index),
+                ID = rng.Next(100, 1000),
+                description = "Test"
+            })
+            .ToArray();
+        }
+    }
+}
